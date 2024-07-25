f3fe16e0b9498882324bb9249f85b957fa0269f4
mnszd <mndevacc@gmail.com>
2024-07-25 15:44:54 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/mn_3d/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mn_3d
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:mn_3d/app/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
